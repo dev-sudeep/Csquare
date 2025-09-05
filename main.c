0fec1b7a723f8dfdd0217b1caea3c598cd0f765c
dev-sudeep <manasa.sudeep2016@gmail.com>
2025-09-05 09:29:49 +0530

    Rename include to csquare

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include "csquare.h"
+#include <csquare.h>
 int main(){
-    repeat(5) {
+    repeat(5, __i) {
         say("Hello, world! %d repeat\n", __i + 1);
         wait(1);
     }
     say("Hello world has been said 5 times!\n");
     char* result = inpy("print(2 + 2)");
-    say("%s\n", result);
+    say("2 + 2 = %s\n", result);
     return 0;
 
 }
